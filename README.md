4aa93f329c5035a50322539aa52f0ede5cd59aae
Mathis Chagneux <chagneux@gpu3.enst.fr>
2023-02-22 12:45:42 +0100

    adding README

---
+++ README.md
@@ -0,0 +1,5 @@
+# First steps 
+1. Add the images into [train_unprocessed](train_unprocessed)
+2. Start the script [filter_files.py](filter_filter.py) which will create a new 'train' folder with only the valid images from the annotation file
+3. Start learning_from_colibris.py to begin training 
+
--- filter_files.py
+++ filter_files.py
@@ -6,4 +6,4 @@ filenames = pd.read_csv('train.csv').iloc[:,0]
 for filename in filenames:
     image_name = filename.split('train/')[1]
     os.makedirs('train', exist_ok=True)
-    shutil.copy(filename, os.path.join('train2', image_name))
+    shutil.copy(filename, os.path.join('train', image_name))
