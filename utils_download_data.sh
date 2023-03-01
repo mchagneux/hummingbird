f8f51f934650ea8cce40250692c3559f7f2917e5
Pierre Gloaguen <pierre.gloaguen@agroparistech.fr>
2023-03-01 10:58:27 +0100

    lien vers donnees avec test complet

--- utils_download_data.sh
+++ utils_download_data.sh
@@ -6,6 +6,6 @@ rm train.csv test.csv class.csv
 # wget télécharge
 # -0- le met sur la sortie standard de wget le contenu du fichier
 # tar x détare 
-wget -O- "https://seafile.agroparistech.fr/f/6f479e65efb04a00b48c/?dl=1" \
-  | tar x 
+wget -O- "https://seafile.agroparistech.fr/f/8a94b7c9ec3e44c7a3b7/?dl=1" \
+  | tar -f x 
 python3 utils_create_train_test_class_files.py
