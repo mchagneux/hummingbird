75403d08bfbc28743d59506b20a12726e9a7c4df
Mathis Chagneux <chagneux@gpu3.enst.fr>
2023-03-01 11:49:21 +0100

    corrected download script

--- learning_from_colibris.py
+++ learning_from_colibris.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import torch
 from torch.utils.data import DataLoader
 import math
