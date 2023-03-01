6e9149c16140f5376b4e4286fee521eb4d83ccc6
Pierre Gloaguen <pierre.gloaguen@agroparistech.fr>
2023-03-01 12:23:00 +0100

    remove le -f qui ne marche pas sur le serveur

--- utils_download_data.sh
+++ utils_download_data.sh
@@ -6,5 +6,5 @@ rm train.csv test.csv class.csv
 # wget télécharge
 # -0- le met sur la sortie standard de wget le contenu du fichier
 # tar x détare 
-wget -O- "https://seafile.agroparistech.fr/f/8a94b7c9ec3e44c7a3b7/?dl=1" | tar x -f
+wget -O- "https://seafile.agroparistech.fr/f/8a94b7c9ec3e44c7a3b7/?dl=1" | tar x
 python3 utils_create_train_test_class_files.py
